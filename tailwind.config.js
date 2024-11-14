cf18ebaa3d5abf7c5354bc9439067b33b803843c
rajeev <rajeev.mohanty@live.com>
2024-11-15 09:45:20 +1100

    first coomit

---
+++ tailwind.config.js
@@ -0,0 +1,11 @@
+
+module.exports = {
+  darkMode: 'class',
+  content: [
+    './app/**/*.{ts,tsx}',
+    './components/**/*.{ts,tsx}',
+    './node_modules/@rnr/**/*.{ts,tsx}',
+  ],
+  theme: {},
+  plugins: [],
+};
